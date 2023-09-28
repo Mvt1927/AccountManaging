05e0bc5de3484c275ee27564dc34f5ab10d3ae5f
Mvt1927 <viettheog4@gmail.com>
2023-09-28 22:32:16 +0700

    Add project files.

---
+++ AccountManaging/Account.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+
+namespace AccountManaging
+{
+    interface IAccount
+    {
+        int AccountId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        decimal Balance { get; set; }
+
+        void Query();
+
+    }
+    class Account : IAccount
+    {
+        private int accountId;
+        private string firstName;
+        private string lastName;
+        private decimal balance;
+
+        public Account()
+        {
+            AccountId = 0;
+            FirstName = "first name";
+            LastName = "last name";
+            Balance = 0;
+        }
+        public Account(int accountId, string firstName, string lastName, decimal balance)
+        {
+            AccountId = accountId;
+            FirstName = firstName;
+            LastName = lastName;
+            Balance = balance;
+        }
+
+        public int AccountId { get => accountId; set => accountId = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public decimal Balance { get => balance; set => balance = value; }
+
+        public void FillInfo()
+        {
+            Console.Write("Account ID: ");
+            AccountId = int.Parse(Console.ReadLine());
+            Console.Write("First Name: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            LastName = Console.ReadLine();
+            Console.Write("Balance: ");
+            Balance = decimal.Parse(Console.ReadLine());
+        }
+
+        public void Query()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Account ID: {0}", AccountId);
+            Console.WriteLine("First Name: {0}", FirstName);
+            Console.WriteLine("Last Name: {0}", LastName);
+            Console.WriteLine("Balance: {0}", Balance);
+            Console.WriteLine("------------------------------");
+
+        }
+
+    }
+    class AccountList
+    {
+        private ArrayList Accounts = new ArrayList();
+        public void LoadFile()
+        {
+            Console.Write("Nhap ten file de load: ");
+            string fileName = Console.ReadLine();
+            try
+            {
+                FileStream fileIn = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                StreamReader streamReader = new StreamReader(fileIn);
+                Accounts.Clear();
+                string str;
+                while ((str = streamReader.ReadLine()) != null)
+                {
+                    string[] list = str.Split(",");
+                    Account account = new Account(int.Parse(list[0]), list[1], list[2], decimal.Parse(list[3]));
+                    Accounts.Add(account);
+                }
+                streamReader.Close();
+                fileIn.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void SaveFile()
+        {
+
+            Console.Write("Nhap ten file de luu: ");
+            string fileName = Console.ReadLine();
+            try
+            {
+                FileStream fileOut = new FileStream(fileName, FileMode.CreateNew, FileAccess.Write);
+                StreamWriter streamWriter = new StreamWriter(fileOut);
+
+                foreach (Account account in Accounts)
+                {
+                    streamWriter.WriteLine("{0},{1},{2},{3}", account.AccountId, account.FirstName, account.LastName, account.Balance);
+                }
+                streamWriter.Close();
+                fileOut.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void NewAccount()
+        {
+            Account a = new Account();
+            a.FillInfo();
+            Accounts.Add(a);
+        }
+
+        public void Report()
+        {
+            foreach (Account account in Accounts)
+            {
+                account.Query();
+            }
+
+        }
+    }
+}
---
+++ AccountManaging/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace AccountManaging
+{
+    class Program
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("-------------Menu-------------");
+            Console.WriteLine("a.\tAdd ");
+            Console.WriteLine("s.\tSave ");
+            Console.WriteLine("l.\tLoad ");
+            Console.WriteLine("r.\tReport");
+            Console.WriteLine("e|q.\tExit");           
+        }
+        static void Main(string[] args)
+        {
+            AccountList accountList = new AccountList();
+            char choice;
+            do {
+                Menu();
+                Console.Write("Your choice: ");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case 'a':
+                        accountList.NewAccount();
+                        break;
+                    case 's':
+                        accountList.SaveFile();
+                        break;
+                    case 'l':
+                        accountList.LoadFile();
+                        break;
+                    case 'r':
+                        accountList.Report();
+                        break;
+                    case 'q':
+                    case 'e':
+                        break;
+                    default:
+                        Console.WriteLine("Error input again ! ");
+                        break;
+                        
+                }
+            } while (choice != 'e' && choice != 'q');
+        }
+    }
+}
